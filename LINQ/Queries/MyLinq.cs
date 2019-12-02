ab07ab9041744a160b01999094daed568b1366b7
salim akhayad <salim.akhayad@hotmail.com>
2019-12-03 00:36:35 +0100

    Linq Queries

---
+++ LINQ/Queries/MyLinq.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queries
+{
+    public static class MyLinq
+    {
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            
+            foreach(var item in source)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                    // deferred execution
+                }
+            }
+           
+        }
+    }
+}
