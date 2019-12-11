422002381f035d3077a9030148a1beb474236cae
salim akhayad <salim.akhayad@hotmail.com>
2019-12-11 11:27:42 +0100

    impl Edit Restaurants

--- ASP.NET Core Fundamentals/OdeToFood/OdeToFood/OdeToFood/Startup.cs
+++ ASP.NET Core Fundamentals/OdeToFood/OdeToFood/OdeToFood/Startup.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,7 +26,14 @@ namespace OdeToFood
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<OdeToFoodDbContext>(options =>
+            {
+                //.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))); OdeToFoodDb
+                options.UseSqlServer(Configuration.GetConnectionString("OdeToFoodDb"));
+            });
             services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
+
+            
             services.AddRazorPages();
         }
 
